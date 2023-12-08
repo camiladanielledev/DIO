c30d26f2e07ea11850a9a0e38abb388d24367e6d
camiladanielle <camiladanielledeoliveira@gmail.com>
2023-12-08 04:45:03 -0300

    desafio de código

---
+++ desafio.js
@@ -0,0 +1,22 @@
+
+
+let Heroi = ("John")
+let xp = 9200;
+let nível
+
+if (xp<=1000) {nível = ("Ferro")}
+else if (xp >=1001 && xp <=2000 ) {nível = ("Bronze")}
+else if (xp >=2001 && xp <=6000 ) {nível = ("Prata")}
+else if (xp >=6001 && xp <=7000 ) {nível = ("Ouro")}
+else if (xp >=7001 && xp <=8000 ) {nível = ("Platina")}
+else if (xp >=8001 && xp <=9000 ) {nível = ("Ascendente")}
+else if (xp >=9001 && xp <=10000 ) {nível = ("Imortal")}
+else if (xp >=10001) {nível = ("Radiante")}
+
+
+console.log("O Herói de nome" ,Heroi ,"está no nível de",nível);
+
+
+
+
+
